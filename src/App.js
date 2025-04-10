18ba056ff3f48a5c9fedc80a9ba46b0684ebd8fa
sudhirwani <sudhirwani8793@gmail.com>
2025-04-10 12:40:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
+import { useEffect, useState, useMemo } from "react";
+import "./styles.css";
+import { countryData } from "./CountryData";
+import SelectComponent from "./SelectComponent";
+import CountryStateDropdown from "./CountryStateDropdown";
+import HeritageSites from "./HeritageSites";
+export default function App() {
+  const [countryName, setCountryName] = useState("");
 
+  const getCountry = (value) => {
+    setCountryName(value);
+  };
+
+  const getState = (value) => {
+    // setCountryName(value);
+  };
+
+  return (
+    <div className="App">      
+      <CountryStateDropdown getCountry={getCountry} getState={getState} />
+    </div>
+  );
+}
